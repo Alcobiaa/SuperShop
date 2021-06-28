59b23edd2afa043cbc03b49ceb1fccd79ea26ba9
Alcobia <goncaloalcobia1904@gmail.com>
2021-06-28 16:47:48 +0100

    API products controller

---
+++ SuperShop/Controllers/API/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperShop.Data;
+
+namespace SuperShop.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _productsRepository;
+
+        public ProductsController(IProductRepository productsRepository)
+        {
+            _productsRepository = productsRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_productsRepository.GetAll());
+        }
+
+    }
+}
